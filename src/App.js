3225144a118736abb246d1518f70f565ea67b32e
Ansaf Muhammed <anchu56@gmail.com>
2022-01-21 10:48:48 +0530

    Made to-do-app functional

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [toDos,setToDos]=useState([]) //list 
+  const [toDo,setToDo]=useState('') //type status
+  var A = new Date();
+  var weekdays = new Array(7);
+  weekdays[0] = "Sunday";
+  weekdays[1] = "Monday";
+  weekdays[2] = "Tuesday";
+  weekdays[3] = "Wednesday";
+  weekdays[4] = "Thursday";
+  weekdays[5] = "Friday";
+  weekdays[6] = "Saturday";
+  var r = weekdays[A.getDay()];
   return (
     <div className="app">
       <div className="mainHeading">
@@ -9,22 +22,41 @@ function App() {
       </div>
       <div className="subHeading">
         <br />
-        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+        <h2 >Whoop, it's {r} 🌝 ☕  </h2>
       </div>
       <div className="input">
-        <input type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
-      </div>
+        <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>setToDos([...toDos,{id:Date.now(),text :toDo,status:false}])} className="fas fa-plus"></i>
+      </div> 
       <div className="todos">
-        <div className="todo">
+      { toDos.map((obj)=>{
+        
+      return(
+      <div className="todo">
           <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>Rect tutorial</p>
+            <input onChange={(e)=>{
+             console.log(e.target.checked)
+             console.log(obj)
+             setToDos(toDos.filter(obj2=>{
+               if(obj2.id==obj.id){
+                 obj2.status=e.target.checked
+               }
+               return obj2
+             }))
+            }} 
+            value={obj.status} type="checkbox" name="" id="" />
+            <p>{obj.text}</p>
           </div>
           <div className="right">
             <i className="fas fa-times"></i>
           </div>
-        </div>
+        </div> )    })     }
+        {toDos.map((obj)=>{
+          if(obj.status){
+            return(<h1>{obj.text}</h1>)
+          }
+          return null
+        })}
       </div>
     </div>
   );
